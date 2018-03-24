08031b7f24c726ae5bf814609ab5a979b9f17c56
Петрикевич Анастасия <nastya.petrikevich@gmail.com>
2018-03-24 18:18:58 +0300

    Changed ExtensionMethods

---
+++ ExtensionMethods/StringExtention.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    /// <summary>
+    /// Contains Convert methods.
+    /// </summary>
+    public static class StringExtention
+    {
+        /// <summary>
+        /// Convert number from p-system to decimal.
+        /// </summary>
+        /// <param name="source">Number to convert.</param>
+        /// <param name="notation">Number system.</param>
+        /// <returns>Number in decimal system.</returns>
+        public static int ToDecimalConverter(this string source, Notation notation)
+        {
+            int numberInDecimal = 0;
+            int power = 0;
+            for (int i = source.Length - 1; i >= 0; i--)
+            {
+                if (!notation.Elements.Contains(source.ElementAt(i).ToString()))
+                {
+                    throw new ArgumentException(nameof(source));
+                }
+
+                if (!int.TryParse(source.ElementAt(i).ToString(), out int digit))
+                {
+                    digit = ConvertSymbolToDigit(source.ElementAt(i));
+                }
+
+                if (digit >= notation.Basis || digit < 0)
+                {
+                    throw new ArgumentException(nameof(source));
+                }
+
+                numberInDecimal += digit * (int)Math.Pow(notation.Basis, power);
+                power++;
+            }
+
+            return numberInDecimal;
+        }
+
+        /// <summary>
+        /// Convert symbol to number.
+        /// </summary>
+        /// <param name="symbol">Symbol to convert.</param>
+        /// <returns>Number.</returns>
+        private static int ConvertSymbolToDigit(char symbol)
+        {
+            switch (symbol)
+            {
+                case 'A':
+                case 'a':
+                    return 10;
+                case 'B':
+                case 'b':
+                    return 11;
+                case 'C':
+                case 'c':
+                    return 12;
+                case 'D':
+                case 'd':
+                    return 13;
+                case 'E':
+                case 'e':
+                    return 14;
+                case 'F':
+                case 'f':
+                    return 15;
+                default:
+                    return -1;
+            }
+        }
+    }
+
+    /// <summary>
+    ///Describes the number system.
+    /// </summary>
+    public class Notation
+    {
+        private int basis;
+        private const string elements = "0123456789ABCDEFabcdef";
+
+        public Notation(int basis)
+        {
+            Basis = basis;
+        }
+
+        public int Basis
+        {
+            get => basis;
+            set
+            {
+                if (value < 2 || value > 16)
+                {
+                    throw new ArgumentException(nameof(value));
+                }
+                basis = value;
+            }
+        }
+
+        public string Elements => elements;
+    }
+}
---
+++ ExtentionMethods.Tests/StringExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ExtensionMethods;
+using NUnit.Framework;
+
+
+namespace ExtentionMethods.Tests
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        [TestCase("10", 5, ExpectedResult = 5)]
+        public static int ToDecimalConverter_Results(string source, int p)
+        {
+            Notation notation = new Notation(p);
+            return source.ToDecimalConverter(notation);
+        }
+
+        [TestCase("1ACB67", 2)]
+        [TestCase("SA123", 2)]
+        [TestCase("96783", 8)]
+        public static void ToDecimalConverter_ArgumentException(string source, int p) 
+        {
+            Notation notation = new Notation(p);
+            Assert.Throws<ArgumentException>(() => source.ToDecimalConverter(notation));
+        }
+
+    }
+}
